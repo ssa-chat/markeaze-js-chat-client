a05ff993ddf4911358d38ef50c54423a5aeb3dd2
Vladimir Savelyev <rostber@yandex.ru>
2019-11-20 17:34:52 +0300

    Display state of agent assign

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -5,7 +5,6 @@ const View = require('./view').default
 module.exports = {
   store: null, // Store from the main app
   libs: null, // Libraries from the main app
-  currentAgentId: null,
   currentAgent: null,
   agents: [],
   sessionsCount: 0,
@@ -48,6 +47,7 @@ module.exports = {
     this.channel.on('client:entered', this.handlerClientEntered.bind(this))
     this.channel.on('message:new', this.handlerMsg.bind(this))
     this.channel.on('message:resend', this.handlerMsgResend.bind(this))
+    this.channel.on('agent:assign', this.handlerAgentAssign.bind(this))
   },
   handlerReady () {
     this.history = msgStory.getData()
@@ -58,19 +58,22 @@ module.exports = {
   handlerClientEntered (msg) {
     this.setAgents(msg.agents)
     this.sessionsCount = msg.sessionsCount
-    this.currentAgentId = msg.current_agent_id
-    this.currentAgent = this.getAgent(msg.current_agent_id)
-    this.libs.log.push('chat', 'Got ClientEntered', msg)
+    this.setCurrentAgent(msg.current_agent_id)
+    this.libs.log.push('chat', 'ClientEntered', msg)
   },
   handlerMsg (msg) {
     this.parseMsg(msg)
-    this.libs.log.push('chat', 'Got Msg', msg)
+    this.libs.log.push('chat', 'Msg', msg)
     this.view.scrollBottom()
   },
   handlerMsgResend () {
     this.parseMsg(msg)
-    this.libs.log.push('chat', 'Got Resend', msg)
     this.view.scrollBottom()
+    this.setCurrentAgent(msg.current_agent_id)
+    this.libs.log.push('chat', 'Resend', msg)
+  },
+  handlerAgentAssign () {
+    this.setCurrentAgent(msg.agent_id)
   },
   parseMsg (msg) {
     if (msg.agent_id) {
@@ -78,6 +81,13 @@ module.exports = {
       msg.avatar_url = agent ? agent.avatar_url : null
     }
     this.history = msgStory.addData(this.history, msg)
+  },
+  setCurrentAgent (currentAgentId) {
+    if (!currentAgentId && !this.currentAgent) return
+
+    this.currentAgent = this.getAgent(currentAgentId)
+    if (this.currentAgent) this.view.assignAgent()
+    else this.view.unassignAgent()
   },
   setAgents (agents) {
     this.agents = agents.reduce((obj, item) => {
