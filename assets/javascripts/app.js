59aed241858a8442c4a0078b67b3082ce77d429e
Vladimir Savelyev <rostber@yandex.ru>
2020-01-13 15:42:52 +0300

    Bump version

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // plugin methods
 
-  version: '0.0.1',
+  version: '1.0.0',
   store: {}, // Store from the main app
   libs: {}, // Libraries from the main app
   create (locale, options) {
