d57908205c60120536ca954831affab2485edc57
Vladimir Savelyev <rostber@yandex.ru>
2020-01-23 13:22:56 +0300

    Fix translations. Bump version to 1.1.2

--- assets/javascripts/translations.js
+++ assets/javascripts/translations.js
@@ -2,15 +2,15 @@ module.exports = {
   ru: {
     copyright: 'Сделано в Markeaze',
     placeholder: 'Напишите ваше сообщение...',
-    in_stock: 'In stock',
-    out_of_stock: 'Out of stock',
-    view_product: 'View product'
+    in_stock: 'В наличии',
+    out_of_stock: 'Не в наличии',
+    view_product: 'Посмотреть товар'
   },
   en: {
     copyright: 'Powered by Markeaze',
     placeholder: 'Type your message here...',
-    in_stock: 'В наличии',
-    out_of_stock: 'Не в наличии',
-    view_product: 'Посмотреть товар'
+    in_stock: 'In stock',
+    out_of_stock: 'Out of stock',
+    view_product: 'View product'
   }
 }
