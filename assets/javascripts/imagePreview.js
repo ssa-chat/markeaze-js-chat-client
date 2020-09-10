38e10281ea5add49dcc89697fddf155633fb5ea6
Vladimir Savelyev <rostber@yandex.ru>
2020-09-10 09:59:41 +0300

    Download image on preview. Bump version to 1.5.2

--- assets/javascripts/imagePreview.js
+++ assets/javascripts/imagePreview.js
@@ -1,6 +1,7 @@
 const translations = require('./translations')
 const helpers = require('./libs/helpers')
 const domEvent = require('./libs/domEvent')
+const { downlodIcon, closeIcon, rightIcon, leftIcon } = require('./libs/icons')
 
 export default class ImagePreview {
   constructor (view, index, elImages) {
@@ -16,7 +17,7 @@ export default class ImagePreview {
     return translations[this.app.locale][key]
   }
   render () {
-    if (this.elContainer) this.removeContainer()
+    if (this.elContainer) this.remove()
 
     const src = this.elImages.item(this.index).href
     helpers.appendHTML(this.view.elCart, this.template(src, this.index, this.total))
@@ -28,47 +29,65 @@ export default class ImagePreview {
 
     this.bind()
   }
-  bind () {
-    domEvent.add(this.elClose, 'click', () => {
-      this.removeContainer()
-    })
-    domEvent.add(this.elPrev, 'click', () => {
-      if (this.index > 0) this.index--
-      this.render()
-    })
-    domEvent.add(this.elNext, 'click', () => {
-      if (this.index < this.total - 1) this.index++
-      this.render()
-    })
-  }
-  removeContainer () {
+  remove () {
+    this.unbind()
+
     this.elContainer.parentNode.removeChild(this.elContainer)
+    delete this.elContainer
+  }
+  bind () {
+    domEvent.add(this.elClose, 'click', this.remove.bind(this))
+    if (this.elPrev) domEvent.add(this.elPrev, 'click', this.onPrev.bind(this))
+    if (this.elNext) domEvent.add(this.elNext, 'click', this.onNext.bind(this))
+
+    domEvent.add(window, 'keydown', this.onKeyDown.bind(this))
+  }
+  unbind () {
+    domEvent.remove(window, 'keydown', this.onKeyDown.bind(this))
+  }
+  onKeyDown (e) {
+    if (e.key === 'Escape') this.remove()
+    if (e.key === 'ArrowLeft') this.onPrev()
+    if (e.key === 'ArrowRight') this.onNext()
+  }
+  onPrev () {
+    if (!this.elContainer || this.index <= 0) return
+    this.index--
+    this.render()
+  }
+  onNext () {
+    if (!this.elContainer || this.index + 1 >= this.total) return
+    this.index++
+    this.render()
   }
   template (src, index, total) {
+    const prevHtml = total === 1 ? '' : `
+          <div class="mkz-c-preview__prev mkz-c-i-js-prev">
+            ${rightIcon}
+          </div>
+    `
+    const nextHtml = total === 1 ? '' : `
+          <div class="mkz-c-preview__next mkz-c-i-js-next">
+            ${leftIcon}
+          </div>
+    `
     return `
       <div class="mkz-c-preview mkz-c-i-js-container">
-        <div class="mkz-c-preview__close mkz-c-i-js-close">
-          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M16 14.1146L22.6 7.51465L24.4853 9.39998L17.8853 16L24.4853 22.6L22.6 24.4853L16 17.8853L9.39998 24.4853L7.51465 22.6L14.1146 16L7.51465 9.39998L9.39998 7.51465L16 14.1146Z" fill="currentColor"/>
-          </svg>
+        <a class="mkz-c-preview__btn mkz-c-preview__btn_type_download" href="${src}" target="_blank">
+          ${downlodIcon}
+        </a>
+        <div class="mkz-c-preview__btn mkz-c-preview__btn_type_close mkz-c-i-js-close">
+          ${closeIcon}
         </div>
         <div class="mkz-c-preview__content">
           <img src="${src}" alt="" class="mkz-c-preview__img" />
         </div>
         <div class="mkz-c-preview__action">
-          <div class="mkz-c-preview__prev mkz-c-i-js-prev">
-            <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.18154 5L6 8.88906L4.90923 10L5.96244e-08 5L4.90923 -8.95132e-07L6 1.11094L2.18154 5Z" fill="currentColor"/>
-            </svg>
-          </div>
+          ${prevHtml}
           <div class="mkz-c-preview__paginate">
             ${index + 1} ${this.t('paginate')} ${total}
           </div>
-          <div class="mkz-c-preview__next mkz-c-i-js-next">
-            <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M3.81846 5L9.05114e-07 1.11094L1.09077 -2.14589e-07L6 5L1.09077 10L5.65121e-07 8.88906L3.81846 5Z" fill="currentColor"/>
-            </svg>
-          </div>
+          ${nextHtml}
         </div>
       </div>
     `
---
+++ assets/javascripts/libs/icons.js
@@ -0,0 +1,14 @@
+module.exports = {
+  downlodIcon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13 10H18L12 16L6 10H11V3H13V10ZM4 19H20V12H22V20C22 20.2652 21.8946 20.5196 21.7071 20.7071C21.5196 20.8946 21.2652 21 21 21H3C2.73478 21 2.48043 20.8946 2.29289 20.7071C2.10536 20.5196 2 20.2652 2 20V12H4V19Z" fill="currentColor"/>
+</svg>`,
+  closeIcon: `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M16 14.1146L22.6 7.51465L24.4853 9.39998L17.8853 16L24.4853 22.6L22.6 24.4853L16 17.8853L9.39998 24.4853L7.51465 22.6L14.1146 16L7.51465 9.39998L9.39998 7.51465L16 14.1146Z" fill="currentColor"/>
+</svg>`,
+  rightIcon: `<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M2.18154 5L6 8.88906L4.90923 10L5.96244e-08 5L4.90923 -8.95132e-07L6 1.11094L2.18154 5Z" fill="currentColor"/>
+</svg>`,
+  leftIcon: `<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.81846 5L9.05114e-07 1.11094L1.09077 -2.14589e-07L6 5L1.09077 10L5.65121e-07 8.88906L3.81846 5Z" fill="currentColor"/>
+</svg>`
+}
